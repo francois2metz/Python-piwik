2124b22eaa52f84d022e22af2469fda4d1ffeec5
François de Metz <francois@2metz.fr>
2009-06-29 22:28:16 +0200

    add cache for javascript tag

--- piwik/django/processors.py
+++ piwik/django/processors.py
@@ -7,8 +7,17 @@ import xml.sax.saxutils as saxutils
 from piwik.django.models import PiwikSite
 from piwik import PiwikAPI
 
+PIWIK_CACHE = {}
+
 def piwik_processor(request):
     current_site = Site.objects.get_current()
-    id_piwik = PiwikSite.objects.filter(site=current_site.id)
-    piwik = PiwikAPI(settings.PIWIK_URL, settings.PIWIK_TOKEN)
-    return {'piwik_tag' : saxutils.unescape(piwik.getJavascriptTag(id_piwik[0].id_site))}
+    piwik_site = PiwikSite.objects.filter(site=current_site.id)
+    id_piwik = piwik_site[0].id_site
+    try:
+        javascript = PIWIK_CACHE[id_piwik]
+    except KeyError:        
+        piwik = PiwikAPI(settings.PIWIK_URL, settings.PIWIK_TOKEN)
+        javascript = saxutils.unescape(piwik.getJavascriptTag(id_piwik))
+        PIWIK_CACHE[id_piwik] = javascript
+    return {'piwik_tag' : javascript}
+
