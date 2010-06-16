4e0614f5a7c918aaff3acb01ce5f2a46aaee8ef0
François de Metz <francois@2metz.fr>
2010-06-17 00:31:04 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='piwik',
     version='0.3',
-    
     description='Access Piwik in python',
     long_description=open('README.rst').read(),
 
@@ -12,9 +11,9 @@ setup(
     author_email='francois@2metz.fr',
     url='http://forge.2metz.fr/p/python-piwik/',
     
+    license='BSD',
     packages = find_packages(),
 
-    
     install_requires = [
         'simplejson'
     ],
@@ -23,9 +22,12 @@ setup(
     },
 
     classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Operating System :: OS Independent',
         'Topic :: Internet :: Log Analysis',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
     ],
 )
